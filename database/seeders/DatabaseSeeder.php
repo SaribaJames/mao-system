752dd878767803524ca50abd97afc077fb060aa5
SaribaJames <jcbs2023-1467-48989@bicol-u.edu.ph>
2026-07-15 20:09:19 +0800

    add program seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             BarangaySeeder::class,
             AdminSeeder::class,
+            ProgramSeeder::class,
         ]);
     }
 }
